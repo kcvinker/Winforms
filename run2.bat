91d442ae67324b28e8766bde4feb0672b551d84a
--global <kcvinu82@gmail.com>
2023-10-24 00:43:40 +0530

    Removed unwanted files

--- run2.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set wdir="E:/OneDrive Folder/OneDrive/Programming/Odin/Winforms"
-set conemu=E:\cmder\vendor\conemu-maximus5\ConEmu64.exe
-start /D %wdir% %conemu% -run odin run app.odin -file
-exit
