50c8db62aa069b12f6a99b8508561422a8f4e0a6
--global <kcvinu82@gmail.com>
2024-02-19 17:51:49 +0530

    Remove unwanted file

--- make.py
+++
@@ -1,4 +0,0 @@
-
-incpath = ""                       				# IMPORTANT VAR
-dbgcmd = ["odin", "run", "app.odin", "-file"]   # IMPORTANT VAR
-rlscmd = ["odin", "run", "app.odin", "-file"]   # IMPORTANT VAR
